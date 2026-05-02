1080ce14c2744a8ad78bfa4aafc2822cd8423fe4
targol <alirezaghasemi7777@gmail.com>
2026-05-02 19:46:54 +0330

    update project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>{
+  return (
+    <div className="App">
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+    </div>
+  );
+};
+
+export default App;
