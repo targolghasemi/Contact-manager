f1ab70fbab7d2822f74eb875b0a66b0486a949ab
Targol Ghasemi <targolghasemi85@gmail.com>
2026-05-07 15:47:02 +0330

    Initial preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () =>{
   return (
     <div className="App">
+      
+      <span className='fa fa-user'></span>
       <h3>اپلیکیشن مدیریت مخاطبین</h3>
+      <button className='btn btn-primary'> button</button>
     </div>
   );
 };
